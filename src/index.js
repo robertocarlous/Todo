7691aa4f282e5b9f134a50e6b31b2ddfd73dc494
robertocarlous <robbertabimbola21@gmail.com>
2023-11-27 13:13:47 +0100

    update to github

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan');
 const TodoRoutes = require("./routes/todo");
 const UserRoutes = require("./routes/userroute");
-const session = require("express-session");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "SECRET_KEY",
-    cookie:{maxAge:40*40*1000},
-    rolling: true,
-  })
-);
+
 app.use("/", TodoRoutes);
 app.use("/", UserRoutes);
 
